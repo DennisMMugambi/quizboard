413568832cdfa54478d7bc90cca590f0e7f42a18
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2019-02-25 20:14:30 +0300

    add sloppy javascript function to tally the users score

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,30 @@ $(document).ready(function(){
   var four = "";
   var score = 0;
 
+//sigh as you read this i couldn't find a better way.Howver i shall look into it
 function userScore(score){
-
+if(one === "1-1"){
+  score = 25;
+} else {
+  var nothing = "";
+}
+if(two === "2-1"){
+  score = 50;
+} else {
+  var nothing2 = "";
+}
+if(three === "3-4"){
+  score = 75;
+} else {
+  var nothing3 = "";
+}
+if(four === "4-4"){
+  score = 100;
+} else {
+  var nothing4 = "";
+}
+alert(score);
+}
 $("form.quiz").submit(function(event){
 event.preventDefault;
   one = $("input:radio[name=one]:checked").val();
