4b93d3674ceb43221b39d046b5c448e51d48b497
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2019-02-25 20:01:11 +0300

    adjust form values to shorter easier to use values

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,20 @@
 $(document).ready(function(){
+  var one = "";
+  var two = "";
+  var three = "";
+  var four = "";
+  var score = 0;
 
+function userScore(score){
 
 $("form.quiz").submit(function(event){
 event.preventDefault;
-var one = $("input:radio[name=one]:checked").val();
-var two = $("input:radio[name=two]:checked").val();
-var three = $("input:radio[name=three]:checked").val();
-var four = $("input:radio[name=four]:checked").val();
+  one = $("input:radio[name=one]:checked").val();
+  two = $("input:radio[name=two]:checked").val();
+  three = $("input:radio[name=three]:checked").val();
+  four = $("input:radio[name=four]:checked").val();
 
-var score =""
-
-if(one === "Javascript is a web scripting language" && two === "var varName" && three === "alert" && four === "func();"){
-  score = 100;
-} else if (one === "Javascript is a web scripting language" || two === "var varName"){
-  score = 25;
-} else if (three === "alert" || four === "func();"){
-  score = 25;
-} else if (one === "Javascript is a web scripting language" && two === "var varName"){
-  score = 50;
-} else if (three === "alert" && four === "func();"){
-  score = 50;
-}
-alert("form has been submitted successfully");
-alert(score);
-});
+  alert("form has been submitted successfully");
+  userScore(score);
+  });
 });
