7349fb6b62b7b534b5eee7db9597fcaded8d0c3f
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2019-02-25 20:46:13 +0300

    fix indentation in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,37 +6,38 @@ $(document).ready(function(){
   var score = 0;
 
 //sigh as you read this i couldn't find a better way.Howver i shall look into it
-function userScore(score){
-if(one === "1-1"){
-  score = 25;
-} else {
-  var nothing = "";
-}
-if(two === "2-1"){
-  score = 50;
-} else {
-  var nothing2 = "";
-}
-if(three === "3-4"){
-  score = 75;
-} else {
-  var nothing3 = "";
-}
-if(four === "4-4"){
-  score = 100;
-} else {
-  var nothing4 = "";
-}
-alert(score);
-}
-$("form.quiz").submit(function(event){
-event.preventDefault;
-  one = $("input:radio[name=one]:checked").val();
-  two = $("input:radio[name=two]:checked").val();
-  three = $("input:radio[name=three]:checked").val();
-  four = $("input:radio[name=four]:checked").val();
+  function userScore(score){
+    if(one === "1-1"){
+      score = 25;
+    } else {
+      var nothing = "";
+    }
+    if(two === "2-1"){
+      score = 50;
+    } else {
+      var nothing2 = "";
+    }
+    if(three === "3-4"){
+      score = 75;
+    } else {
+      var nothing3 = "";
+    }
+    if(four === "4-4"){
+      score = 100;
+    } else {
+      var nothing4 = "";
+    }
+    alert("Your score is" + score + " !");
+  }
+  $("form.quiz").submit(function(event){
+    event.preventDefault;
+    one = $("input:radio[name=one]:checked").val();
+    two = $("input:radio[name=two]:checked").val();
+    three = $("input:radio[name=three]:checked").val();
+    four = $("input:radio[name=four]:checked").val();
 
-  alert("form has been submitted successfully");
-  userScore(score);
-  });
+    userScore(score);
+    $(".userscore").show();
+    $("#insertscore").text(score);
+    });
 });
