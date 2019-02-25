f2d162a7697a82ed59677d3cd284e2c44012ae3d
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2019-02-25 11:39:51 +0300

    correct questions for there to be only one answer

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,18 @@
 $(document).ready(function(){
+
+
 $("form.quiz").submit(function(event){
 event.preventDefault;
-var score = "";
-$("input [radio="#1-1"]:checked").click(function(){
-  score = 1;
-  alert("correct");
-});
-$("input #2-1").click(function(){
-  score += 1;
-  alert("correct");
-});
-$("input #3-3").click(function(){
-  score += 1;
-  alert("correct");
-});
-$("input #4-4").click(function(){
-  score += 1;
-  alert("correct");
-});
+var one = $("input:radio[name=one]:checked").val();
+var two = $("input:radio[name=two]:checked").val();
+var three = $("input:radio[name=three]:checked").val();
+var four = $("input:radio[name=four]:checked").val();
+
+alert(one);
+alert(two);
+alert(three);
+alert(four);
 alert("form has been submitted successfully");
+
 });
 });
